ab319f2a84d086c82492aa4cf8b8eee697f75bd9
sumi.byun <selina2000@naver.com>
2022-03-10 04:03:54 +0900

    Update 1074.py

--- 1074.py
+++ 1074.py
@@ -1,31 +1,26 @@
-def func(n, point,x,y) :
-    num = n//2
-    # | 0 | 1 |
-    # | 2 | 3 |
-    # 0번 영역
-    if num + x > point[0] and num + y > point[1]:
-        index = 0
-    # 1번 영역
-    elif num + x > point[0]:
-        index = 1
-    # 2번 영역
-    elif num + y > point[1]:
-        index = 2
-    # 3번 영역
+def solutuion(N, r,c):
+    #탈출
+    if N == 0:
+        return 0
+    N -=1
+    result = 0
+    # 1
+    if r <  2**N and c < 2 ** N:
+        result = solutuion(N,r,c)
+    # 2
+    elif r < 2** N :
+        c -= 2**N
+        result = solutuion(N,r,c) + (2**N) ** 2 * 1
+    #3
+    elif c < 2 **N:
+        r -= 2 ** N
+        result = solutuion(N,r,c) + (2**N) ** 2 * 2
     else:
-        index = 3
-    print(n,index,point,x,y)
-    if n == 2 :
-        # 탈출 조건
-        index = point[0] %2 * 2 + point[1] %2
-        return index
-    else :
-        if index%2==1:
-            x += num
-        if index>=2 :
-            y +=num
-        return func(num, point,x ,y) + (num ** 2) * index
+        r -= 2 ** N
+        c -= 2 ** N
+        result = solutuion(N,r,c) + (2**N) ** 2 * 3
+    return result
 
 if __name__ == '__main__':
     N, r,c = map(int, input().split(" "))
-    print(func(2**N, (r,c),0,0))
+    print(solutuion(N,r,c))
